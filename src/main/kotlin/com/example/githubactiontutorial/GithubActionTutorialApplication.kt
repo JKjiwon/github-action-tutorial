815ba170aa98554911c6b31e8f99cdd2b24eea73
JKjiwon <jwkim.oa@gmail.com>
2023-05-25 13:40:11 +0900

    feat : spring project 추가

---
+++ src/main/kotlin/com/example/githubactiontutorial/GithubActionTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubactiontutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubActionTutorialApplication>(*args)
+}
