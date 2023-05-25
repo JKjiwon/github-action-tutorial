2abdba0c1b9da6cfd3c452b2e59c31ee5fd14df7
JKjiwon <jwkim.oa@gmail.com>
2023-05-25 16:48:21 +0900

    feat : 로그 추가

--- src/main/kotlin/com/example/githubactiontutorial/GithubActionTutorialApplication.kt
+++ src/main/kotlin/com/example/githubactiontutorial/GithubActionTutorialApplication.kt
@@ -1,11 +1,16 @@
 package com.example.githubactiontutorial
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class GithubActionTutorialApplication
 
+private val log : Logger = LoggerFactory.getLogger(GithubActionTutorialApplication::class.java)
+
 fun main(args: Array<String>) {
 	runApplication<GithubActionTutorialApplication>(*args)
+	log.info("GithubActionTutorialApplication start")
 }
